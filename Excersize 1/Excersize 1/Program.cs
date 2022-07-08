1b924ad6da171adf707c31cabab8ee7d1d46fb5b
Jordan Lockwood <8xavienlockwood8@gmail.com>
2022-07-08 11:20:28 -0600

    first commit

---
+++ Excersize 1/Excersize 1/Program.cs
@@ -0,0 +1,56 @@
+﻿for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+for (int i = 0; i <5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+for (int i = 0; i < 5; i++) 
+{
+    Console.WriteLine($"{i}");
+}
